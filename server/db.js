6e4a3d1000a5768ffa03bebf1b403e686bef9a2a
Bidesh Paul <74422903+bideshpaul05@users.noreply.github.com>
2024-02-10 08:47:24 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -9,7 +9,3 @@ export const db = mysql.createConnection({
     port: 3306
 })
 
-// host:"sql.freedb.tech",
-// user:"freedb_bidesh_2001",
-// password:"&!Cw7QA@4s4xm3m",
-// database:"freedb_inventory_i"
