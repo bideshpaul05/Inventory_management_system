e570446977359f555fd7a7c68973e995a3bac2b8
bidesh <paulbidesh2001@gmail.com>
2024-02-10 09:32:50 +0530

    commit2

--- client/src/pages/Home_feed.jsx
+++ client/src/pages/Home_feed.jsx
@@ -140,7 +140,7 @@ function Home_feed() {
           <div className="content">
             {data && data.map((item, ind) => {
               console.log(item.cat)
-              if (cat === "all" || cat === item.cat)
+              if (cat === "all" || cat === item.cat && ind<=5)
               
                 return (
                   <div className="item_container" key={item.id}>
